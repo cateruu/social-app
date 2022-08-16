7962d52a4101877969ef931077a64c507c0605dd
Paweł Kromołowski <cater981pl@gmail.com>
2022-08-16 21:18:24 +0200

    showing user image in post input

--- components/PostInput/PostInput.tsx
+++ components/PostInput/PostInput.tsx
@@ -8,11 +8,15 @@ import styles from './PostInput.module.css';
 
 import TextareaAutosize from 'react-textarea-autosize';
 import { BsCardImage } from 'react-icons/bs';
+import { MdCancel } from 'react-icons/md';
 
 const PostInput: NextPage = () => {
   const themeCtx = useContext(ThemeContext);
 
   const [textInput, setTextInput] = useState<string>('');
+  const [selectedFile, setSelectedFile] = useState<HTMLInputElement | null>(
+    null
+  );
 
   return (
     <section
@@ -39,7 +43,17 @@ const PostInput: NextPage = () => {
             themeCtx.theme === 'light' ? styles.lightText : null
           }`}
         />
-        <div className={styles.buttons}>
+        <div className={styles.selectedImageContainer}>
+          <Image
+            src='https://i.pinimg.com/474x/ec/e2/b0/ece2b0f541d47e4078aef33ffd22777e.jpg'
+            layout='fill'
+            quality={100}
+            alt='user image'
+            className={styles.selectedImage}
+          />
+          {/* <MdCancel /> */}
+        </div>
+        {/* <div className={styles.buttons}>
           <div className={styles.inserts}>
             <input type='file' />
           </div>
@@ -47,7 +61,7 @@ const PostInput: NextPage = () => {
             <div className={`${styles.button} ${styles.fake}`}>Post</div>
           )}
           {textInput && <button className={styles.button}>Post</button>}
-        </div>
+        </div> */}
       </div>
     </section>
   );
