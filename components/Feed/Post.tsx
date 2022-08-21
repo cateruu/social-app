45682d20b739a9a9accfd53d60b22e24732d5fdf
Paweł Kromołowski <cater981pl@gmail.com>
2022-08-21 14:26:56 +0200

    deleting user posts

--- components/Feed/Post.tsx
+++ components/Feed/Post.tsx
@@ -1,4 +1,9 @@
-import { DocumentSnapshot, QueryDocumentSnapshot } from 'firebase/firestore';
+import {
+  deleteDoc,
+  doc,
+  DocumentSnapshot,
+  QueryDocumentSnapshot,
+} from 'firebase/firestore';
 import { NextPage } from 'next';
 import Image from 'next/image';
 import styles from './Post.module.css';
@@ -7,7 +12,13 @@ import { PostType } from './Feed';
 
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import { FaRegComment } from 'react-icons/fa';
+import { FiTrash2 } from 'react-icons/fi';
+
 import { useTheme } from '../../store/theme-context';
+import { useUser } from '@auth0/nextjs-auth0';
+import { MouseEvent } from 'react';
+import { db } from '../../config/firebase';
+import { useRouter } from 'next/router';
 
 interface Post {
   id: string;
@@ -17,6 +28,12 @@ interface Post {
 
 const Post: NextPage<Post> = ({ id, post, postPage }) => {
   const { theme } = useTheme();
+  const router = useRouter();
+
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
   return (
     <section className={`${styles.post} ${theme === 'light' && styles.light}`}>
@@ -51,6 +68,17 @@ const Post: NextPage<Post> = ({ id, post, postPage }) => {
             <FaRegComment />
             <span className={styles.amount}>777</span>
           </div>
+          {user?.sub === post.id && (
+            <div className={`${styles.iconContainer} ${styles.delete}`}>
+              <FiTrash2
+                onClick={(e: MouseEvent) => {
+                  e.stopPropagation();
+                  deleteDoc(doc(db, 'posts', id));
+                  router.push('/');
+                }}
+              />
+            </div>
+          )}
         </div>
       </div>
     </section>
